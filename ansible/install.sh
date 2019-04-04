0512b335c0504af0e9c298df78417cb50b116be1
Amogha Suresh <amoghavs@gmail.com>
2019-04-04 08:34:16 +0000

    The config which works with all the componenets on controller, 1-8 are actions

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,7 +4,7 @@ ansible-playbook -i environments/distributed setup.yml
 echo "Commented docker in prereq_build.yml"
 ansible-playbook -i environments/distributed prereq_build.yml 
 ansible-playbook -i environments/distributed registry.yml
-cd ../ && sudo ./gradlew distDocker -PdockerRegistry=172.24.66.133:5000
+cd ../ && sudo ./gradlew distDocker -PdockerRegistry=serverless-controller:5000
 cd ansible
 ansible-playbook -i environments/distributed couchdb.yml
 ansible-playbook -i environments/distributed initdb.yml
@@ -13,4 +13,5 @@ ansible-playbook -i environments/distributed openwhisk.yml
 ansible-playbook -i environments/distributed postdeploy.yml
 ansible-playbook -i environments/distributed apigateway.yml
 ansible-playbook -i environments/distributed routemgmt.yml
-wsk property set --auth $(cat files/auth.whisk.system) --apihost serverless-1
+wsk property set --auth $(cat files/auth.guest) --apihost serverless-controller
+wsk property set --auth $(cat files/auth.whisk.system) --apihost serverless-controller
